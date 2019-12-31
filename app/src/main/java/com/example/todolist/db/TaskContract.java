9ce2359ea4b789d9dbaab08d221e6b3c259519f1
Jay <jial8@uw.edu>
2019-12-30 16:02:38 -0800

    Initial commit

---
+++ app/src/main/java/com/example/todolist/db/TaskContract.java
@@ -0,0 +1,15 @@
+package com.example.todolist.db;
+
+import android.provider.BaseColumns;
+
+/** defines constants which used to access the data in the database **/
+public class TaskContract {
+    public static final String DB_NAME = "com.aziflaj.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
